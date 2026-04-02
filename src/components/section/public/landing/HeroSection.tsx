dd7f10edd50460a1ebb50bf4c0b970723c0fbf33
Mulia Andiki <muliaandiki@gmail.com>
2026-04-02 16:51:22 +0700

    feat: ready for develop fe

---
+++ src/components/section/public/landing/HeroSection.tsx
@@ -0,0 +1,9 @@
+const LandingHeroSection = () => {
+  return (
+    <section className="w-full flex items-center min-h-screen justify-center">
+      <h1>Init Landing Desktop</h1>
+    </section>
+  );
+};
+
+export default LandingHeroSection;
